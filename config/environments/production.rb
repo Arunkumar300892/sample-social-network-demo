18a23ec89a4fb0dec6087d34fa6f1c0fe8dc9928
Arunkumar300892 <arunkumar300892@gmail.com>
2018-12-29 02:06:40 +0530

    [New] Configurational changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # mailer configurations
+  config.action_mailer.default_url_options = { host: ENV['HOST_NAME'] }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['MAILER_USERNAME'],
+    :password => ENV['MAILER_PASSWORD'],
+    :domain => ENV['MAILER_DOMAIN'],
+    :address => ENV['SMTP_MAILER'],
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
